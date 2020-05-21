5bcb535f59d007bac1f7a873cd8b7c677ca154a8
bethanie.garfin <bethanie.garfin@autotrader.co.uk>
2020-05-21 19:39:13 +0100

    Add express server attempt 2

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+const static_path = path.join(__dirname, '..', 'public')
+
+app.use(express.static(static_path))
+
+app.get('/', (req, res) => res.sendFile(path.join(static_path, 'index.html')))
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
